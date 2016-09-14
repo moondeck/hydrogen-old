0f6f7de112e1ed03eb37fb3ff4d700c51f0e8779
Olgierd Nowakowski <olgierd@novakovsky.eu>
2016-09-14 13:33:16 +0200

    test

---
+++ configure.sh
@@ -0,0 +1,3 @@
+#/bin/sh
+echo "Hello! I am a dummy configure script, until my master, m00nd3ck makes a better one, possibly with autoconf."
+echo "he is lazy thou, so dont expect that to happen soon."
